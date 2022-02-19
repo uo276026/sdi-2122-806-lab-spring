48447f4f5cba71dedff69e3381092c6c643ea785
Lara <uo276026@uniovi.es>
2022-02-19 17:31:46 +0100

    SDI-2122-806-3.5-Validación de datos en el servidor

--- src/main/java/com/uniovi/notaneitor/controllers/UsersController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/UsersController.java
@@ -1,10 +1,13 @@
 package com.uniovi.notaneitor.controllers;
 
 import com.uniovi.notaneitor.services.SecurityService;
+import com.uniovi.notaneitor.validators.SignUpFormValidator;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.notaneitor.entities.*;
 import com.uniovi.notaneitor.services.UsersService;
@@ -12,6 +15,8 @@ import org.springframework.security.core.Authentication;
 
 @Controller
 public class UsersController {
+    @Autowired
+    private SignUpFormValidator signUpFormValidator;
 
     @Autowired
     private UsersService usersService;
@@ -63,11 +68,23 @@ public class UsersController {
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signup(@ModelAttribute("user") User user, Model model) {
+    public String signup(@Validated User user, BindingResult result) {
+        signUpFormValidator.validate(user, result);
+        if (result.hasErrors()) {
+            return "signup";
+        }
         usersService.addUser(user);
         securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
         return "redirect:home";
     }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(Model model) {
         return "login";
