0be65f766daa4625a9515296ad80a89e2955e0bc
Lara <uo276026@uniovi.es>
2022-03-07 20:06:49 +0100

    SDI-2122-806-5.7-Casos Uso VistaPrivada.

---
+++ src/test/java/com/uniovi/notaneitor/pageobjects/PO_LoginView.java
@@ -0,0 +1,13 @@
+package com.uniovi.notaneitor.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class PO_LoginView extends PO_NavView{
+
+
+    public static void fillLoginForm(WebDriver driver, String dni, String s1) {
+
+    }
+}
